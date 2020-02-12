1b9e60be0fad14d3003324d55cd38012b38c8bf8
Massimo Siani <massimo.siani@bitrock.it>
2020-02-12 16:57:35 +0100

    remove any reference to geostream

--- cypress/integration/app-title.spec.js
+++ cypress/integration/app-title.spec.js
@@ -3,7 +3,7 @@
 before(() => cy.visit('/'));
 
 describe('App title heading', () => {
-  it('Should display a text of Kafka Geostream', () => {
-    cy.get('#app-title').should('contain', 'Kafka Geostream');
+  it('Should display a text of Bitrock DVS', () => {
+    cy.get('#app-title').should('contain', 'Bitrock DVS');
   });
 });
