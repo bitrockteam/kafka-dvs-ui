459dadc16df38939fcb6e395d01f5606bf583cfc
Simone Ripamonti <simone.ripamonti@bitrock.it>
2020-02-14 09:08:42 +0100

    Allow using externally provided API/WS endpoints

--- src/libs/endpoints.ts
+++ src/libs/endpoints.ts
@@ -2,5 +2,4 @@ const WS: string = process.env.VUE_APP_DVS_WS_URL || 'WSS_URL';
 const REST: string = process.env.VUE_APP_DVS_REST_URL || 'REST_URL';
 
 export const streamWS = (value: string): string => `${WS}/stream/${value}`;
-
 export const restAPI = (value: string): string => `${REST}/v1/rest/${value}`;
