ec7478f51037ab86dc945b249bce11861f23a08d
vadimbakaev <bakaev.vad@gmail.com>
2020-02-28 12:09:35 +0100

    Add top-list-item component, update amber

--- src/components/top-five-list.vue
+++ src/components/top-five-list.vue
@@ -2,13 +2,13 @@
   <div class="top-five-list">
     <div class="title-section">{{ titleSection }}</div>
     <div v-if="data.length">
-      <amber-progress-bar
+      <top-list-item 
               v-for="(item, index) in data"
               :key="index"
-              :label="item.name"
-              :value="item.percent"
-              :display="item.count"
-              :format="item.format || ''"
+              :name="item.name"
+              :percent="item.percent"
+              :count="item.count"
+              :format="item.format"            
       />
     </div>
     <loading-placeholder-stat v-else />
@@ -18,11 +18,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import LoadingPlaceholderStat from '@/components/loading-placeholder-stat.vue';
-import '@amber-ds/components/progress-bar';
+import TopListItem from '@/components/top-list-item.vue';
 
 @Component({
   name: 'top-five-list',
   components: {
+    TopListItem,
     LoadingPlaceholderStat,
   },
   props: ['titleSection', 'loading'],
---
+++ src/components/top-list-item.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="top-list-item">
+        <amber-progress-bar
+              onmouseover="" 
+              style="cursor: pointer;"
+              @click="select"
+              :label="name"
+              :value="percent"
+              :display="count"
+              :format="format"
+              :disabled=!selected
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import '@amber-ds/components/progress-bar';
+
+@Component({
+    name: 'top-list-item',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        percent: {
+            type: Number,
+            required: true,
+        },
+        count: {
+            type: Number,
+            required: true,
+        },
+        format: {
+            type: String,
+            required: false,
+            default: '',
+        },
+    },
+    data() {
+        return {
+            selected: true,
+        };
+    },
+    methods: {
+        select() {
+            this.$data.selected = !this.$data.selected;
+        },
+    },
+})
+export default class extends Vue {
+}
+
+</script>
